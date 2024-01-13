55e76b4d7c9fc38a90d5fb01f69f85d6f01627f4
Sorwest <151322116+Sorwest@users.noreply.github.com>
2024-01-13 20:51:18 -0300

    fixed soggins duo artifact.

---
+++ ExternalAPI/FrogproofManager.cs
@@ -0,0 +1,22 @@
+﻿using Sorwest.CorrosiveCobra.Cards;
+
+namespace Sorwest.CorrosiveCobra;
+
+internal sealed class FrogproofManager : IFrogproofHook
+{
+    public FrogproofManager()
+    {
+        Manifest.Instance.SogginsApi?.RegisterFrogproofHook(this, 0);
+    }
+
+    public FrogproofType? GetFrogproofType(State state, Combat? combat, Card card, FrogproofHookContext context)
+    {
+        if (card is CobraCardSlimeSogginsDuoBotch || card is CobraCardSlimeSogginsDuoDouble)
+            return FrogproofType.Innate;
+        return null;
+    }
+
+    public void PayForFrogproof(State state, Combat? combat, Card card)
+    {
+    }
+}
